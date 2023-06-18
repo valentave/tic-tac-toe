edbb6c71e5bb2b9cbbc84b1e42bd3b0e6471ec26
Valentave <valentintaverna99@gmail.com>
2023-06-18 00:41:59 -0300

    Add functionality to play vs CPU
    A feature has been added where player 1 or player 2's turn
    (not both) can decide the play randomly by the CPU.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const main = document.querySelector("main");
 const form = document.querySelector("#form-election");
 const playerOneElection = document.getElementsByName("player-one");
 const playerTwoElection = document.getElementsByName("player-two");
+const playerOneManage = document.getElementsByName("manage-one");
+const playerTwoManage = document.getElementsByName("manage-two");
 const gridCells = document.querySelectorAll(".grid-cell");
 const gameBoard = Array.from(gridCells).reduce((acc, cell, index) => {
     const rowIndex = Math.floor(index / 3);
@@ -15,8 +17,8 @@ const gameBoard = Array.from(gridCells).reduce((acc, cell, index) => {
     return acc;
 }, []);
 
-let playerOne = PlayerFactory("Player one");
-let playerTwo = PlayerFactory("Player two");
+let playerOne = PlayerFactory();
+let playerTwo = PlayerFactory();
 let game = GameFactory();
 
 // ######### Functions
@@ -54,7 +56,7 @@ function isThereAWinner() {
     return false;
 }
 
-// Check if a game is over
+// Check if the game is over
 function isGameOver() {
     let result = -1;
     if (game.getTurn() === 9) result = 0;
@@ -111,17 +113,37 @@ function playTurn(choice, cell) {
     }
 }
 
+// Función para que la CPU juegue su turno
+function playCPUTurn(player) {
+    const availableCells = Array.from(gridCells).filter(isCellFree);
+    const randomIndex = Math.floor(Math.random() * availableCells.length);
+    const randomCell = availableCells[randomIndex];
+    player.play(randomCell);
+}
+  
+// Función para verificar si es el turno de la CPU
+function isCPUTurn() {
+    if (playerOne.getName() === "CPU"){
+        return game.getTurn() % 2 === 0 && !isGameOver()
+    }
+    if (playerTwo.getName() !== "CPU"){
+        return game.getTurn() % 2 !== 0 && !isGameOver();
+    }
+    return false
+}
+
+
 // ######### Objects
 
-function PlayerFactory(name) {
+function PlayerFactory() {
     let choice = "";
-    const setChoice = (x) => {
-        choice = x;
-    }
+    let name = "";
+    const setName = (userName) => name = userName;
+    const setChoice = (option) => choice = option;
     const getChoice = () => choice;
     const getName = () => name;
     const play = (cell) => playTurn(choice, cell); 
-    return {setChoice, getChoice, getName, play}
+    return {setName, setChoice, getChoice, getName, play}
 }
 
 function GameFactory() {
@@ -134,49 +156,81 @@ function GameFactory() {
 
 // ######### Events
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
     event.preventDefault();
     // Set election for player one
     for (let i = 0; i < playerOneElection.length; i++) {
         if (playerOneElection[i].checked) {
             playerOne.setChoice(playerOneElection[i].classList.value);
             break;
-            }
+        }
     }
     // Set election for player two
     for (let i = 0; i < playerTwoElection.length; i++) {
         if (playerTwoElection[i].checked) {
             playerTwo.setChoice(playerTwoElection[i].classList.value);
             break;
-            }
+        }
     }
+    // Set player or CPU in player one
+    for (let i = 0; i < playerOneManage.length; i++) {
+        if (playerOneManage[i].checked) {
+            playerOne.setName(playerOneManage[i].classList.value);
+            break;
+        }
+    }
+    // Set player or CPU in player two
+    for (let i = 0; i < playerTwoManage.length; i++) {
+        if (playerTwoManage[i].checked) {
+            playerTwo.setName(playerTwoManage[i].classList.value);
+            break;
+        }
+    }
+
     // Check if both elections are different
     if (playerOne.getChoice() === playerTwo.getChoice()) {
         alert("Select different options for both players.");
         return;
+    } else if (playerOne.getName() === playerTwo.getName() &&
+        playerOne.getName() === "CPU"){
+        alert("Both players cannot be CPU.");
+        return;
     } else {
         form.classList.add("hidden");
     }
-});
 
-// Display a play
-gridCells.forEach((cell) => {
+    // If the first turn should be CPU's, play CPU's turn
+    if (playerOne.getName() === "CPU" && playerTwo.getName() !== "CPU") {
+        playCPUTurn(playerOne);
+    } 
+    // Start the game
+    playGame();
+  });
 
-    cell.addEventListener ("click", () => {
-        if (game.getTurn() % 2 === 0) playerOne.play(cell);
-        else playerTwo.play(cell);
-    })
-})
 
-// Reset the game when press reset button
-main.addEventListener("click", (event) => {
-    if (event.target.classList.contains("btn-reset")) {
-        gridCells.forEach((cell) => {
-            cell.textContent = "";
-        });
-        game.resetTurns();
-        const resultContainer = document.querySelector(".result-container");
-        main.removeChild(resultContainer);
-        form.classList.remove("hidden");
-    }
-});
+// Play the Tic Tac Toe game
+function playGame() {
+    // Display a play
+    gridCells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        if (playerOne.getName() === "Player" && !isCPUTurn() && game.getTurn() % 2 === 0) {
+            playerOne.play(cell);
+            if (isGameOver() === -1 && playerTwo.getName() === "CPU") {
+                playCPUTurn(playerTwo);
+            }
+        }
+        if (playerTwo.getName() === "Player" && !isCPUTurn() && game.getTurn() % 2 !== 0) {
+            playerTwo.play(cell);
+            if (isGameOver() === -1 && playerOne.getName() === "CPU") {
+                playCPUTurn(playerOne);
+            }
+        }
+      });
+    });
+      // Reset the game when the reset button is pressed
+      main.addEventListener("click", (event) => {
+        if (event.target.classList.contains("btn-reset")) {
+            location.reload();
+        }
+    });
+}
